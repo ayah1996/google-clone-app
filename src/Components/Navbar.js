5a66f547d65b570ae1f4ad54fd7366569137f25d
ayah1996 <ayah.o.ezzeddine@gmail.com>
2022-04-07 04:11:49 +0300

    fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,9 +14,9 @@ const Navbar = ({ darkTheme, setDarkTheme }) => {
         <button
           type="button"
           onClick={() => setDarkTheme(!darkTheme)}
-          className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-l hover:shadow-lg"
+          className="text-xl dark:text-gray-900 px-2 py-l "
         >
-          {darkTheme ? "Light 💡 " : "Dark 🌙"}
+          {darkTheme ? " 💡 " : " 🌙"}
         </button>
       </div>
       <Search />
