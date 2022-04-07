9eb95e6ba0dd4c03ba3dcae5524c341ff7f057c8
ayah1996 <ayah.o.ezzeddine@gmail.com>
2022-04-08 02:18:06 +0300

    fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,6 +5,8 @@ import Search from "./Search";
 const Navbar = ({ darkTheme, setDarkTheme }) => {
   return (
     <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
+      {process.env.REACT_APP_TEST}
+
       <div className="flex justify-between items-center space-x-5 w-screen ">
         <Link to="/">
           <p className="text-xl text-blue-500 font-bold ">Goggle</p>
