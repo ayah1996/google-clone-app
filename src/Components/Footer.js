01c4425ab25215d2962d2cff397c6b2e9128f598
ayah1996 <ayah.o.ezzeddine@gmail.com>
2023-09-24 06:30:00 +0300

    test

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,9 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
-      {/* <h1> 2023 Googl, Inc.</h1> */}
-    </div>
+    <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200"></div>
   );
 };
 
